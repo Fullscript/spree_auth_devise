fe85c0d1e9cef22fff7cba5a5f514a8fd5d2c72c
Damien Imberdis <damien@ndstorefront.com>
2013-07-08 11:13:27 -0400

    Rescue from DestroyWithOrdersError only if it's defined.

--- app/controllers/spree/admin/admin_users_controller_decorator.rb
+++ app/controllers/spree/admin/admin_users_controller_decorator.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../../base_controller_decorator', __FILE__)
 Spree::Admin::UsersController.class_eval do
-  rescue_from Spree::User::DestroyWithOrdersError, :with => :user_destroy_with_orders_error
+  if Spree.user_class.const_defined?("DestroyWithOrdersError")
+    rescue_from "#{Spree.user_class}::DestroyWithOrdersError".constantize, :with => :user_destroy_with_orders_error
+  end
 
   update.after :sign_in_if_change_own_password
 
